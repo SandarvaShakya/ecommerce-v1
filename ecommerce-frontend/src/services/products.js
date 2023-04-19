1909779d4c4ffec7affe892ce82d1a37b65c94f5
SandarvaShakya <sandarvashakya34@gmail.com>
2023-04-19 14:18:36 +0545

    🛠Update baseUrl

--- ecommerce-frontend/src/services/products.js
+++ ecommerce-frontend/src/services/products.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/supplier/products'
+const baseUrl = 'https://ecommerce-sss.vercel.app/api/supplier/products'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
