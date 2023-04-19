443d28da4785164caba4583603edbdb4c898a2ef
SandarvaShakya <sandarvashakya34@gmail.com>
2023-04-19 14:14:28 +0545

    ➕Add vercel configurations

--- ecommerce-backend/index.js
+++ ecommerce-backend/index.js
@@ -84,6 +84,12 @@ app.delete('/api/supplier/products/:id', (request, response) => {
         })
 })
 
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+  
+app.use(unknownEndpoint)
+
 const PORT = process.env.PORT
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
